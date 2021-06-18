4f7d99c0789213043521c8337332427bc8e2f5e1
thebrokencoder <theextraordinaryrobotician@gmail.com>
2021-06-18 11:40:39 +0530

    Add files via upload

--- eNFA.py
+++ eNFA.py
@@ -11,6 +11,15 @@ eclosure = {0:[0],1:[1],2:[2]}
 inputSymbols = ['a','b','c']
 currentState = []
 
+def getEClosure():
+    temp =[]
+    for key in trans:
+        if 'e' in trans[key]:
+            eclosure[key].append(trans[key]['e'])
+            if 'e' in trans[trans[key]['e']]:
+                eclosure[key].append(trans[trans[key]['e']]['e'])               
+    print(eclosure)
+        
 def initializationNFA(inputString):
     inputString = inputString.replace(" ","")    
     validSequence = True
@@ -31,47 +40,81 @@ def getNextState(current):
             return trans[state]    
     
 def enfa(sequence):
-    while len(sequence) > 0 :
-        if len(currentState) == 1 :
+    if len(sequence) == 0:
+        while len(currentState) > 0:
             current = currentState.pop(0)
-            print("Current State is => " ,current)
-            print("Current input symbol =>",sequence[0])
+            print("Current State ", current)
             nextState = getNextState(current)
-            print("next available states = ",nextState)
-            for value in nextState:
-                if value == sequence[0]:
-                    print("Next State =>" ,nextState[value])
-                    currentState.append(nextState[value])
+            print("Next State",nextState)
+            currentState.append(nextState['e'])
+            print("Next upcoming state ",currentState)
+            if currentState[0] == finalState[0]:
+                print("String is accepted with Epsilon Transitions")
+                currentState.clear()
+                return
             else:
-                if sequence[0] not in nextState.keys():
-                    currentState.append(nextState['e'])
-                    print("Epsilon Transition Occuring here..")
-                    eclosure[current].append(nextState['e']) 
-                    if len(currentState) == 0:
+                continue
+    else:
+        while len(sequence) > 0 :
+            if len(currentState) == 1 :
+                current = currentState.pop(0)
+                print("Current State is => " ,current)
+                print("Current input symbol =>",sequence[0])
+                nextState = getNextState(current)
+                print("next available states = ",nextState)
+                for value in nextState:
+                    if value == sequence[0]:
+                        print("Next State =>" ,nextState[value])
+                        currentState.append(nextState[value])
+                else:
+                    if sequence[0] not in nextState.keys():
+                        if 'e' not in nextState.keys():
+                            sequence.pop(0)
+                            if len(sequence)==0:
+                                print("String is Invalid as per the NFA")
+                                return
+                            else:
+                                currentState.append(current)
+                        else:    
+                            currentState.append(nextState['e'])
+                            print("Epsilon Transition Occuring here..")
+                            eclosure[current].append(nextState['e']) 
+                            if len(currentState) == 0:
+                                sequence.pop(0)
+                                print(sequence)
+                    else:
                         sequence.pop(0)
                         print(sequence)
-                else:
-                    sequence.pop(0)
-                    print(sequence)
+                    
+    
+        
                     
     if currentState[0] == finalState[0]:
-        print("string is valid as per the DFA")
+        print("string is valid as per the NFA")
         print("Epsilon closures :" ,eclosure)
+        currentState.clear()
+        print(currentState)
     else:
-        print("String is not valid as per the DFA")
+        print("String is not valid as per the NFA")
     
 def enfaRun():
-    option = int(input ("Select the option : 1.Test String in eNFA 2. Exit : "))
+    option = int(input ("Select the option : 1.Test String in eNFA 2. Exit 3.getEClosure : "))
     if option == 1:
         inputString = str(input("Enter the pattern in a,b,c : "))
         initializationNFA(inputString)
         enfaRun()
     elif option == 2:
         return "Thanks"
+    elif option == 3:
+        getEClosure()
     else:
         print("Please enter a valid input")
         enfaRun()
 enfaRun()
+
+
+    
+    
 
 
             
